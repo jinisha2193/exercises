ecaac40f3cbdd1bad5ea876c9aa886a60e11c88c
jinisha2193 <jinisha2193@gmail.com>
2016-01-16 21:51:31 +0530

    question 5
    question 5

---
+++ ExQuestion5/src/com/digitek/exercises/DisplayValues.java
@@ -0,0 +1,20 @@
+package com.digitek.exercises;
+
+public class DisplayValues {
+	
+    
+	
+	public static void main(String args[])
+	{
+		
+		int a = 10;
+		float b = 12.5f;
+		String c = "Java Programming";
+		System .out.println("Integer value is : " +a);
+		System .out.println("Float value is : " +b);
+		System .out.println("String value is : " +c);
+		
+	}
+	
+
+}
